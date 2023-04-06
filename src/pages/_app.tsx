b57a506022b977dc627dda675245dba3c24f69ce
John <jgz6@uakron.edu>
2023-04-06 14:18:14 -0400

    added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,16 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import type { AppProps } from 'next/app'
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps} >
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
-};
+}
+
 
 export default api.withTRPC(MyApp);
