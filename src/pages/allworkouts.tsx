019b27c9fa353da38b209b9b07fe92c42c45366b
John <jgz6@uakron.edu>
2023-04-18 10:25:07 -0400

    nav bar added

--- src/pages/allworkouts.tsx
+++ src/pages/allworkouts.tsx
@@ -30,7 +30,15 @@ const Home: NextPage = () => {
       </Head>
       <main className="text-white text-center flex min-h-screen   flex-col  bg-gradient-to-b from-[#000000] to-[#44454b]">
         <div>
-          <SignedIn>
+<nav className="flex items-center justify-between flex-wrap bg-black-500 p-6">
+  <div className="flex items-center flex-shrink-0 text-white mr-6">
+  </div>
+  <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div className="text-sm lg:flex-grow">
+      <Link href="makeplan" className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">Edit Workout Plan</Link>
+      < Link href="allworkouts" className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">Workout History</Link>
+    </div>
+    <SignedIn>
             <div className="text-white   items-end flex p-6 items-right flex-col ">
                 <UserButton appearance={{ 
                   elements: { 
@@ -38,6 +46,10 @@ const Home: NextPage = () => {
                     }
                   }} />
             </div>
+    </SignedIn>
+  </div>
+</nav>
+          <SignedIn>
           <br></br>
           <Content />
           <br></br>
